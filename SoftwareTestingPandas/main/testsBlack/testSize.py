ffa6e6886597e4801be51547723420aedbf648fc
Julia Ekman <julia.ekman97@gmail.com>
2020-11-25 15:05:30 +0100

    Added test2

--- SoftwareTestingPandas/main/testsBlack/testSize.py
+++ SoftwareTestingPandas/main/testsBlack/testSize.py
@@ -13,21 +13,38 @@ Contributors:
 '''
 
 '''
-This file contains multiple black box tests for the function groupby from the library Pandas. 
+This file contains multiple black box tests for the function size() from the library Pandas. 
 
-A groupby operation involves some combination of splitting the object, applying a function, and combining the results. 
-This can be used to group large amounts of data and compute operations on these groups.
+Return an int representing the number of elements in this object. Return the number of rows if Series. Otherwise return the number of rows times number of columns if DataFrame.
+In this test, only a dataframe is used.  
 '''
 class TestPandasTitanic(unittest.TestCase):
     """
     Test suite for the sensitive edge problem
     """
-    
+      
     def setUp(self):
         self.df = pd.read_csv ('../src/data/titanic.csv')
-
-    def testSizeOriginal(self):
+        self.emptydf = pd.DataFrame()
+    '''
+    test1 compares the regular output when the dataset is applied to the size function. It tests the basic funtionality of size(). 
+    The input is the dataset called "df".
+    The output should be equal to 7096. 
+    '''         
+    def test1(self):
         self.assertEqual(self.df.size,7096)
+
+    """
+    test2 checks if the size of an empty dataframe is equal to zero. 
+    The input is called "emptydf" and is an empty dataset. 
+    The output should be equal to zero. 
+    """
+    def test2(self):
+        self.assertTrue(self.emptydf)
+        self.assertEqual(self.emptydf.size, 0)
+    """
+    testa om det funkar fortfarande om det finns NANvärden? 
+    """
 
 if __name__ == '__main__' :
     unittest.main()
