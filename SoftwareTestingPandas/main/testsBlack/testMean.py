b0ac381d9cb875ee5c6f864ab986726ab524eff5
Helena Frestadius <helenafrestadius@hotmail.se>
2020-11-30 09:43:07 +0100

    added a few more tests and a short description

--- SoftwareTestingPandas/main/testsBlack/testMean.py
+++ SoftwareTestingPandas/main/testsBlack/testMean.py
@@ -13,9 +13,9 @@ Contributors:
 '''
 
 '''
-This file contains multiple black box tests for the function '..' from the library Pandas. 
+This file contains multiple black box tests for the function DataFrame.mean() from the library Pandas. 
 
-The function '..' does the following....
+The mean function computes the mean of a the vaules from a requested axis. 
 '''
 class TestPandasTitanic(unittest.TestCase):
     """
@@ -25,8 +25,35 @@ class TestPandasTitanic(unittest.TestCase):
     def setUp(self):
         self.df = pd.read_csv ('../src/data/titanic.csv')
 
-    def testMean(self):
+    #General case 
+    def test1(self):
         self.assertEqual(self.df['Age'].mean(), 29.471443066516347)
+
+    #With nan as first value
+    def test2(self):
+        self.df['Age'][0]= np.nan
+        self.assertEqual(self.df['Age'].mean(), 29.479875846501127)
+
+    #With nan as first and last value
+    def test3(self):
+        self.df['Age'][0]= np.nan
+        self.df['Age'][self.df.index[-1]]= np.nan
+        self.assertEqual(self.df['Age'].mean(), 29.47702824858757)
+
+    #mean of column with only nan and skipna = False
+    def test4(self):
+        self.df['nan']= np.nan
+        self.assertFalse(self.df['nan'].mean(skipna=False) == np.nan)
+
+    #TypeError
+    def test5(self):
+        with self.assertRaises(TypeError):
+            self.df['Name'].mean()
+    
+    #test with negative values
+    def test6(self):
+        self.df['Age'] = -self.df['Age']
+        self.assertEqual(self.df['Age'].mean(),-29.471443066516347)
 
 
 if __name__ == '__main__' :
