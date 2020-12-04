88cc259862a41031a5f2c9c4993beadeadf9c394
Martin Dannelind <36402231+dannemarre@users.noreply.github.com>
2020-12-04 13:21:56 +0100

    rm

--- SoftwareTestingPandas/main/src/pandas/validate_bool_kwarg.py
+++
@@ -1,8 +0,0 @@
-def validate_bool_kwarg(value, arg_name):
-    """ Ensures that argument passed in arg_name is of type bool. """
-    if not (is_bool(value) or value is None):
-        raise ValueError(
-            f'For argument "{arg_name}" expected type bool, received '
-            f"type {type(value).__name__}."
-        )
-    return value
