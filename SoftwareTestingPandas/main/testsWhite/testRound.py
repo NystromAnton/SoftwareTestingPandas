2f52599948f23b405e758c163d839f45235c5c1b
Anders <anton1nystrom@gmail.com>
2020-12-15 15:41:30 +0100

    bättre tester för round

--- SoftwareTestingPandas/main/testsWhite/testRound.py
+++ SoftwareTestingPandas/main/testsWhite/testRound.py
@@ -26,20 +26,82 @@ Using a Series, the number of places for specific columns can be specified with 
 
 '''
 class TestPandasRound(unittest.TestCase):
-    
+    # TODO byt så det inte är cats o dogs som i deras exempel
     def setUp(self):      
-        self.df = pd.DataFrame({'col1': [1.2340303, 0.203, 9.0000], 'col2': [3.43245, 4.0, 288.349983], 'col3': [3.45, 4.52345, 4.002348937], 'col4': [3.4335, 4.55, 87.34829]})
+        self.df = pd.DataFrame({\
+            'col1': [decimal.Decimal('1.2340303'), decimal.Decimal('0.203'), decimal.Decimal('9.0000')], \
+            'col2': [decimal.Decimal('3.43245'), decimal.Decimal('4.0'), decimal.Decimal('288.349983')], \
+            'col3': [decimal.Decimal('3.45'), decimal.Decimal('4.52345'), decimal.Decimal('4.002348937')], \
+            'col4': [decimal.Decimal('3.4335'), decimal.Decimal('4.55'), decimal.Decimal('87.34829')]})
        
 
-    def testOneDecimal(self):
-        data = self.df            
+    def testFirst(self):
+        df = pd.DataFrame([(.21, .32), (.01, .67), (.66, .03), (.21, .18)], columns=['dogs', 'cats'])        
+        df = df.round(1)    
+        for column in df.iteritems():  
+            for value in column[1].values:           
+                self.assertEqual(str(value)[::-1].find('.'), 1)
+
+
+    def testDict(self): #TODO Den kollar bara att det är en decimal men den bort också kolla att för cats är det bara nollor som decimal
+        df = pd.DataFrame([(.21, .32), (.01, .67), (.66, .03), (.21, .18)], columns=['dogs', 'cats'])
+        #print(df)      
+        df = df.round({'dogs': 1, 'cats': 0}) 
+        #print(df)
+        for column in df.iteritems():  
+            for value in column[1].values:           
+                self.assertEqual(str(value)[::-1].find('.'), 1)
+
+    def testSeries(self): #Det här testet adderade rad 8061 och 8062 
+        """
+        Tests using a series to specify different number of decimal places
+        for different columns. Done by indexing by column names and decimal places as values.   
+
+        Tests that values not of type integer or float gets returned as is without rounding. 
+        """        
+        df = pd.DataFrame([(.21, .32), (.01, .67), (.66, .03), (.21, .18)], columns=['dogs', 'cats'])      
+        decimals = pd.Series([0, 1], index=['cats', 'dogs'])         
+        df = df.round(decimals)          
+        for column in df.iteritems():  #TODO Den kollar bara att det är en decimal men den bort också kolla att för cats är det bara nollor som decimal
+            for value in column[1].values:           
+                self.assertEqual(str(value)[::-1].find('.'), 1)
+
+        # This added rows 8049 and 8050
+        df2 = pd.DataFrame([('val1', 'val2'), ('val3', 'val4'), ('val5', 'val6'), ('val7', 'val8')], columns=['col1', 'col2'])
+        decimals2 = pd.Series([0, 1], index=['col1', 'col2'])
+        print(df2)
+        df2 = df2.round(decimals2)
+        print(df2)
+
+
+    def testErrors(self):
+        """
+        Asserts that not unique indexing raises ValueError.
+        Asserts that decimals not of type integer, dict-like or Series raises TypeError
+        Ass
+        """
+        df = pd.DataFrame([(.21, .32), (.01, .67), (.66, .03), (.21, .18)], columns=['dogs', 'cats'])
+        
+        decimals = pd.Series([0, 1], index=['cats', 'cats'])         
+        with self.assertRaises(ValueError):
+            df = df.round(decimals)
+
+        decimals = "string"
+        with self.assertRaises(TypeError):
+            df = df.round(decimals)       
+           
+
+    def stestOneDecimal(self):
+        data = self.df
+        print(data)            
         data = data.round(1)          
+        print(data)
         for column in data.iteritems():  
             for value in column[1].values:           
                 self.assertEqual(str(value)[::-1].find('.'), 1)
 
 
-    def testTwoDecimals(self):
+    def stestTwoDecimals(self):
         data = self.df        
         data = data.round(2)               
         for column in data.iteritems():  
@@ -47,7 +109,7 @@ class TestPandasRound(unittest.TestCase):
                 self.assertTrue(str(value)[::-1].find('.') <= 2)
 
 
-    def testMoreDecimals(self): 
+    def stestMoreDecimals(self): 
         data = self.df     
         print(data)   
         data = data.round(8)  
@@ -62,7 +124,7 @@ class TestPandasRound(unittest.TestCase):
                     self.assertTrue(str(value)[::-1].find('.') <= 8)
 
 
-    def testNoArgument(self): 
+    def stestNoArgument(self): 
         data = self.df        
         data = data.round()               
         for column in data.iteritems():              
