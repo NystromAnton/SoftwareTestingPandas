901abea849a96a2e50c5b675762b8528a47b1a7a
Anders <anton1nystrom@gmail.com>
2020-12-15 16:34:42 +0100

    Changed column name internally

--- SoftwareTestingPandas/main/testsWhite/testRound.py
+++ SoftwareTestingPandas/main/testsWhite/testRound.py
@@ -28,12 +28,12 @@ Using a Series, the number of places for specific columns can be specified with 
 class TestPandasRound(unittest.TestCase):
     # TODO byt så det inte är cats o dogs som i deras exempel
     def setUp(self):      
-        np.random.seed(25)
-        self.df = pd.DataFrame(np.random.random([3, 4]), columns =["A", "B", "C", "D"]) 
+        np.random.seed(120)
+        self.df = pd.DataFrame(np.random.random([3, 4]), columns =["1", "2", "3", "4"]) 
        
 
     def testFirst(self):
-        df = pd.DataFrame([(.239, .532), (.265, .982), (.902, .347), (.128, .298)], columns=['dogs', 'cats'])        
+        df = pd.DataFrame([(.239, .532), (.265, .982), (.902, .347), (.128, .298)], columns=['col1', 'col2'])        
         df = df.round(1)    
         for column in df.iteritems():  
             for value in column[1].values:           
@@ -41,9 +41,9 @@ class TestPandasRound(unittest.TestCase):
 
 
     def testDict(self): #TODO Den kollar bara att det är en decimal men den bort också kolla att för cats är det bara nollor som decimal
-        df = pd.DataFrame([(.21, .32), (.01, .67), (.66, .03), (.21, .18)], columns=['dogs', 'cats'])
+        df = pd.DataFrame([(.239, .532), (.265, .982), (.902, .347), (.128, .298)], columns=['col1', 'col2'])
         #print(df)      
-        df = df.round({'dogs': 1, 'cats': 0}) 
+        df = df.round({'col1': 1, 'col2': 0}) 
         #print(df)
         for column in df.iteritems():  
             for value in column[1].values:           
@@ -56,23 +56,23 @@ class TestPandasRound(unittest.TestCase):
 
         Tests that values not of type integer or float gets returned as is without rounding. 
         """        
-        df = pd.DataFrame([(.21, .32), (.01, .67), (.66, .03), (.21, .18)], columns=['dogs', 'cats'])      
-        decimals = pd.Series([0, 1], index=['cats', 'dogs'])         
+        df = pd.DataFrame([(.239, .532), (.265, .982), (.902, .347), (.128, .298)], columns=['col1', 'col2'])     
+        decimals = pd.Series([0, 1], index=['col2', 'col1'])         
         df = df.round(decimals)          
         for column in df.iteritems():  #TODO Den kollar bara att det är en decimal men den bort också kolla att för cats är det bara nollor som decimal
             for value in column[1].values:           
                 self.assertEqual(str(value)[::-1].find('.'), 1)
        
-        df2 = pd.DataFrame([('val1', 'val2'), ('val3', 'val4'), ('val5', 'val6'), ('val7', 'val8')], columns=['col1', 'col2'])
-        decimals2 = pd.Series([0, 1], index=['col1', 'col2'])        
+        df2 = pd.DataFrame([('val1', 'val2'), ('val3', 'val4'), ('val5', 'val6'), ('val7', 'val8')], columns=['A', 'B'])
+        decimals2 = pd.Series([0, 1], index=['A', 'B'])        
         df3 = df.round(decimals2) # This ads rows 8049-8050    
         df4 = df2.round(decimals2) # This ads row 8055
 
 
     def stestForValueNotIntegerOrFloat(self):
-        df = pd.DataFrame([("foo", .32), (.01, .67), (.66, .03), (.21, .18)], columns=['dogs', 'cats'])
+        df = pd.DataFrame([("foo", .323), (.013, .677), (.666, .083), (.291, .118)], columns=['col1', 'col2'])
         
-        decimals = pd.Series([0, 1], index=['dogs', 'cats'])                 
+        decimals = pd.Series([0, 1], index=['col1', 'col2'])                 
         df = df.round(decimals)
         print(df)
 
@@ -82,9 +82,9 @@ class TestPandasRound(unittest.TestCase):
         Asserts that decimals not of type integer, dict-like or Series raises TypeError
         Ass
         """
-        df = pd.DataFrame([(.21, .32), (.01, .67), (.66, .03), (.21, .18)], columns=['dogs', 'cats'])
+        df = pd.DataFrame([(.239, .532), (.265, .982), (.902, .347), (.128, .298)], columns=['col1', 'col2'])
         
-        decimals = pd.Series([0, 1], index=['cats', 'cats'])         
+        decimals = pd.Series([0, 1], index=['col2', 'col2'])         
         with self.assertRaises(ValueError):
             df = df.round(decimals)
 
